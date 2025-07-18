96715289bc2dcdca74e971f942a127bc7ba1dfa2
Martin Markov <marto98nm@gmail.com>
2025-07-18 17:31:35 +0300

    implamented log-in functionality

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,15 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from './user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  form: FormGroup;
   showPassword: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
 
   togglePassword(): void {
     this.showPassword = !this.showPassword;
+  }
+
+  login(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const { email, password } = this.form.value;
+
+    this.userService.login(email!, password!)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          alert(err?.error?.error || 'Login  failed. Please try again.')
+        }
+      });
   }
 }
